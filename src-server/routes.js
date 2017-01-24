a44b24968e5a34c61501971d33efbcf221fad924
IlSima <simone.lacarpia@gmail.com>
2017-01-24 09:36:45 +0100

    hapi.js + vue.js
    configuration without css loader

---
+++ src-server/routes.js
@@ -0,0 +1,15 @@
+
+var baseR = require('./routing/baseRouting');
+
+
+module.exports = [].concat(baseR);
+
+
+/*****DA UTILIZZARE IN SEGUITO START****/
+/*
+var cart = require('./cart');
+var user = require('./user');
+
+module.exports = [].concat(cart, user);
+*/
+/*****DA UTILIZZARE IN SEGUITO END******/
